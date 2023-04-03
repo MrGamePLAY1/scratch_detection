bc5950d01bfb1b7e25cbef7a18d4fedd4bced574
Craig Sheehy <craigsheehy123@gmail.com>
2023-04-03 13:57:55 +0100

    added ML for images

--- functions.py
+++ functions.py
@@ -3,6 +3,9 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import keras
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
 
 # ------------------ Test Print ------------------
 def test_print():
@@ -100,3 +103,69 @@ def length_of_line(x1, y1, x2, y2):
     length = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
     print('this should be updated')
     return length
+
+
+# ------------------- Keras Model Train --------------------
+def keras_model_train():
+    train_dir = 'images/sample/heavy', 'images/sample/light'
+    eval_dir = 'images/sample/audi'
+    test_dir = 'images/sample/audi'
+
+    # Model parameters
+    batch_size = 32
+    img_height = 180
+    img_width = 180
+    num_epochs = 10
+    learning_rate = 0.001
+
+    # Define the data augmentation and preprocessing techniques
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+    val_datagen = ImageDataGenerator(rescale=1. / 255)
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+    # Load the data
+    train_generator = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(img_height, img_width),
+        batch_size=batch_size,
+        class_mode='binary')
+
+    val_data = val_datagen.flow_from_directory(
+        eval_dir,
+        target_size=(224, 224),
+        batch_size=batch_size,
+        class_mode='categorical'
+    )
+    test_data = test_datagen.flow_from_directory(
+        test_dir,
+        target_size=(224, 224),
+        batch_size=batch_size,
+        class_mode='categorical'
+    )
+
+    # Create the model
+    model = keras.models.Sequential([
+        keras.layers.Conv2D(16, 3, padding='same', activation='relu', input_shape=(img_height, img_width, 3)),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Conv2D(32, 3, padding='same', activation='relu'),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Conv2D(64, 3, padding='same', activation='relu'),
+        keras.layers.MaxPooling2D(),
+        keras.layers.Flatten(),
+        keras.layers.Dense(512, activation='relu'),
+        keras.layers.Dense(1)
+    ])
+
+    # Compile the model
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
